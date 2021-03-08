f76ddd14a4c361f200d1a9969d739f748f2d4df1
Oğuzhan Yılmaz <oguzhan.yilmaz@arfitect.com>
2021-03-09 00:44:49 +0300

    UnitOfWork implemented for repositories

---
+++ MyBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Data.Abstract
+{
+    public interface IUnitOfWork:IAsyncDisposable
+    {
+        IPostRepository Posts { get; }
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; }// _unitOfWork.Categories.AddAsync();
+        // _unitOfWork.Users.AddAsync();
+        // _unitOfWork.SaveAsync();
+        Task<int> SaveAsync();
+
+
+    }
+}
---
+++ MyBlog.Data/Concrete/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyBlog.Data.Abstract;
+using MyBlog.Data.Concrete.EntityFramework.Contexts;
+using MyBlog.Data.Concrete.EntityFramework.Repositories;
+
+namespace MyBlog.Data.Concrete
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        private readonly MyBlogContext _context;
+        private EfPostRepository _postRepository;
+        private EfCategoryRepository _categoryRepository;
+        private EfCommentRepository _commentRepository;
+        private EfUserRepository _userRepository;
+        private EfRoleRepository _roleRepository;
+
+        public UnitOfWork(MyBlogContext context)
+        {
+            _context = context;
+        }
+
+        
+
+        public IPostRepository Posts => _postRepository ?? new EfPostRepository(_context);
+        public ICategoryRepository Categories => _categoryRepository ?? new EfCategoryRepository(_context);
+        public ICommentRepository Comments => _commentRepository ?? new EfCommentRepository(_context);
+        public IRoleRepository Roles => _roleRepository ?? new EfRoleRepository(_context);
+        public IUserRepository Users => _userRepository ?? new EfUserRepository(_context);
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+    }
+}
