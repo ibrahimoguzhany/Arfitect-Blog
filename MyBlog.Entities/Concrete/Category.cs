3c40f9cf6abb19ff78ccb72bc8f4d67de2645f3e
Oğuzhan Yılmaz <oguzhan.yilmaz@arfitect.com>
2021-03-08 18:53:34 +0300

    EfEntityRepositoryBase & IEntityRepository added

---
+++ MyBlog.Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyBlog.Shared.Entities.Abstract;
+
+namespace MyBlog.Entities.Concrete
+{
+    public class Category:EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ MyBlog.Entities/Concrete/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyBlog.Shared.Entities.Abstract;
+
+namespace MyBlog.Entities.Concrete
+{
+    public class Comment:EntityBase,IEntity
+    {
+        public string Text { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+
+    }
+}
