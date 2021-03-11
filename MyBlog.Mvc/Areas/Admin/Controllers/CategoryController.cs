0231a1d991f2bd66b8c7c6508af2e7659c09a876
Ibrahim Oguzhan Yilmaz <ibrahimoguzhany@gmail.com>
2021-03-11 15:55:06 +0300

    Update methods setted. CategoryUpdateDto modal added.

--- MyBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
+++ MyBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -59,6 +59,17 @@ namespace MyBlog.Mvc.Areas.Admin.Controllers
             return Json(categoryAddAjaxErrorModel);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Update(int categoryId)
+        {
+            var result = await _categoryService.GetCategoryUpdateDto(categoryId);
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return PartialView("_CategoryUpdatePartial", result.Data);
+            }
+            return NotFound();
+        }
+
         public async Task<JsonResult> GetAllCategories()
         {
             var result = await _categoryService.GetAllByNoneDeleted();
