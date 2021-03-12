29fa0add96a5c0a012d696905ff7cd5798a7fc64
Ibrahim Oguzhan Yilmaz <ibrahimoguzhany@gmail.com>
2021-03-12 23:27:58 +0300

    UserMenuViewComponent added

---
+++ MyBlog.Mvc/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyBlog.Entities.Concrete;
+
+namespace MyBlog.Mvc.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public User User { get; set; }
+    }
+}
