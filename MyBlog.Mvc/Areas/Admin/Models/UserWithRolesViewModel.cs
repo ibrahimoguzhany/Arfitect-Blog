2c8a8f8012307378f857d3e5e91997ef1d68ca10
Ibrahim Oguzhan Yilmaz <ibrahimoguzhany@gmail.com>
2021-03-12 23:20:48 +0300

    AdminMenuViewComponent added

---
+++ MyBlog.Mvc/Areas/Admin/Models/UserWithRolesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyBlog.Entities.Concrete;
+
+namespace MyBlog.Mvc.Areas.Admin.Models
+{
+    public class UserWithRolesViewModel
+    {
+        public User User { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
