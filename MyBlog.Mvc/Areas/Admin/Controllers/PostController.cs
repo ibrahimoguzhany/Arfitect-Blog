ff7418e4376ce000ab8695c54708fff2c6be5efc
Ibrahim Oguzhan Yilmaz <ibrahimoguzhany@gmail.com>
2021-03-13 20:35:59 +0300

    Commit before changing folderNames

---
+++ MyBlog.Mvc/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PostController : Controller
+    {
+        [HttpGet]
+        public async IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
