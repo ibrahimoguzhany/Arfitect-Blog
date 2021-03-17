7e4bd5dd7a636042fb8c9c00f1bd24a9f9b18a9b
Ibrahim Oguzhan Yilmaz <ibrahimoguzhany@gmail.com>
2021-03-17 13:21:17 +0300

    PostDetailtViewModels added

--- ArfitectBlog.Mvc/Controllers/PostController.cs
+++ ArfitectBlog.Mvc/Controllers/PostController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ArfitectBlog.Data.Concrete;
+using ArfitectBlog.Entities.ComplexTypes;
 using ArfitectBlog.Mvc.Models;
 using ArfitectBlog.Services.Abstract;
 using ArfitectBlog.Shared.Utilities.Results.ComplexTypes;
@@ -41,8 +42,26 @@ namespace ArfitectBlog.Mvc.Controllers
             var postResult = await _postService.GetAsync(postId);
             if (postResult.ResultStatus == ResultStatus.Success)
             {
+                var userPosts = await _postService.GetAllByUserIdOnFilter(
+                    userId: postResult.Data.Post.UserId,
+                    filterBy: FilterBy.Category, 
+                    orderBy: OrderBy.Date, 
+                    isAscending: false, 
+                    takeSize: 10,
+                    categoryId: postResult.Data.Post.CategoryId, 
+                    startAt: DateTime.Now, endAt: DateTime.Now,
+                    minViewCount: 0, maxCommentCount: 99999, minCommentCount: 0, maxViewCount: 99999);
                 await _postService.IncreaseViewCountAsync(postId);
-                return View(postResult.Data);
+                return View(new PostDetailViewModel()
+                {
+                    PostDto = postResult.Data,
+                    PostDetailRightSideBarViewModel = new PostDetailRightSideBarViewModel
+                    {
+                        PostListDto = userPosts.Data,
+                        Header = "Kullanıcının Aynı Kategori Üzerindeki En Çok Okunan Makaleleri",
+                        User=postResult.Data.Post.User
+                    },
+                });
             }
 
             return NotFound();
---
+++ ArfitectBlog.Mvc/Models/PostDetailRightSideBarViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArfitectBlog.Entities.Concrete;
+using ArfitectBlog.Entities.Dtos;
+
+namespace ArfitectBlog.Mvc.Models
+{
+    public class PostDetailRightSideBarViewModel
+    {
+        public string Header { get; set; }
+        public PostListDto PostListDto { get; set; }
+        public User User { get; set; }
+
+    }
+}
